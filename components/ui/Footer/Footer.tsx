d9bf2eded4c0409abe4c42c864ca23d7faccf1b7
Nico Hevia <50466554+nhevia@users.noreply.github.com>
2022-06-29 02:18:42 +0200

    removes bulma and refactors everything that used it with css (#8)

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="content has-text-centered">
+    <footer className={s.footer}>
+      <div className={s['footer-text-centered']}>
         <p>
           <strong>XMarket</strong> by <a href="https://nhevia.com">Nhevia</a>.
           The source code is licensed
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-import '../styles/bulma-variables.scss';
 import type { AppProps } from 'next/app';
 import Layout from '@components/common/Layout';
 
