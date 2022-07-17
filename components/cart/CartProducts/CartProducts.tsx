f258c80a210659753ec051447c907f4d53238c6e
nhevia <nicolashevia@hotmail.com>
2022-07-18 00:40:41 +0200

    feat: adds CartProducts component

---
+++ components/cart/CartProducts/CartProducts.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useCartStore } from 'store/cart';
+import s from './CartProducts.module.css';
+
+const CartProducts = () => {
+  const { products, quantity, total } = useCartStore((state) => state);
+
+  return (
+    <div className={s.root}>
+      <p className={s.title}>
+        Order Summary
+        <span className={s.description}> ({quantity} items)</span>
+      </p>
+      <p className={s.total}>TOTAL ${total}</p>
+      {products?.map((p) => (
+        <div key={p.title} className={s.product}>
+          <img src={p.thumbnail} alt={p.title} />
+          <div className={s['product-description']}>
+            <p className={s.name}>
+              {p.title} <span className={s.description}>({p.count})</span>
+            </p>
+            <p className={s.price}>${(p.count * p.price).toFixed(2)}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CartProducts;
