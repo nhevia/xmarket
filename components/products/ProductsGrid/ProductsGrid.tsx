cb4c25badc3ced2c85552e42154e4f78e1fec63d
nhevia <nicolashevia@hotmail.com>
2022-07-23 23:57:26 +0200

    feat: nothing found feedback on product filtering

--- components/products/ProductsGrid/ProductsGrid.tsx
+++ components/products/ProductsGrid/ProductsGrid.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useFilterStore } from 'store/filters';
 import ProductCard from '../ProductCard/ProductCard';
 import Dots from '@components/ui/Loaders/Dots';
@@ -10,20 +10,43 @@ interface AppProducts {
 }
 
 const ProductsGrid = ({ productsData }: AppProducts) => {
+  const [products, setProducts] = useState<React.ReactNode>([]);
+
   const { filter, isFiltering } = useFilterStore((state) => state);
+
+  useEffect(() => {
+    if (!productsData) return;
+
+    const filtered = productsData?.filter((p) =>
+      p.title.toLowerCase().includes(filter)
+    );
+
+    if (filtered?.length > 0) {
+      setProducts(
+        filtered.map((product: Product) => (
+          <React.Fragment key={product.id}>
+            <ProductCard product={product} />
+          </React.Fragment>
+        ))
+      );
+    } else {
+      setProducts(
+        <>
+          <div className={s.empty}>Nothing found, you might also like ...</div>
+          {productsData.map((product: Product) => (
+            <React.Fragment key={product.id}>
+              <ProductCard product={product} />
+            </React.Fragment>
+          ))}
+        </>
+      );
+    }
+  }, [filter]);
 
   return (
     <>
       {!isFiltering ? (
-        <div className={s.grid}>
-          {productsData
-            ?.filter((p) => p.title.toLowerCase().includes(filter))
-            .map((product: Product) => (
-              <React.Fragment key={product.id}>
-                <ProductCard product={product} />
-              </React.Fragment>
-            ))}
-        </div>
+        <div className={s.grid}>{products}</div>
       ) : (
         <div style={{ marginTop: '15%' }}>
           <Dots color="black" />
