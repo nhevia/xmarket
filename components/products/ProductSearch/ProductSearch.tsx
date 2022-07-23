fe89782b8fdb2a938a4fa9c8f9aee37b3b6d77e6
Nico Hevia <50466554+nhevia@users.noreply.github.com>
2022-07-23 02:22:50 +0200

    Search products #2 (#13)
    * feat: adds a new store for filters
    
    * feat: adds ProductSearch component
    
    * fix: include Layout component inside the react-query client
    
    * feat: filter products by search
    
    * style: unified search box and icon
    
    * feat: filter products by enter or icon click
    
    * feat: apply debounce on product search

--- components/products/ProductSearch/ProductSearch.tsx
+++ components/products/ProductSearch/ProductSearch.tsx
@@ -1,21 +1,62 @@
-import React, { ChangeEvent } from 'react';
-import s from './ProductSearch.module.css';
+import React, { useState } from 'react';
+import { debounce } from 'throttle-debounce';
 import { useFilterStore } from 'store/filters';
+import s from './ProductSearch.module.css';
 
 const ProductSearch = () => {
-  const setFilter = useFilterStore((state) => state.setFilter);
+  const [filterWord, setFilterWord] = useState('');
 
-  const handleFilterChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setFilter(e.target.value);
+  const { setFilter, setIsFiltering } = useFilterStore((state) => state);
+
+  const searchText = (text: string) => {
+    setFilter(text);
+    setIsFiltering(false);
+  };
+  const searchTextDebounce = debounce(1000, searchText);
+
+  const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterWord(e.target.value);
+  };
+
+  const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      setIsFiltering(true);
+      setTimeout(() => {
+        setIsFiltering(false);
+      }, 1001);
+      searchTextDebounce(filterWord);
+    }
+  };
+
+  const onSearchHandle = () => {
+    setIsFiltering(true);
+    setTimeout(() => {
+      setIsFiltering(false);
+    }, 1001);
+    searchTextDebounce(filterWord);
   };
 
   return (
-    <div>
+    <div className={s.root}>
       <input
         placeholder="Search for products"
-        className={s.root}
+        className={s.input}
         onChange={handleFilterChange}
+        onKeyDown={handleEnter}
       />
+      <div onClick={onSearchHandle} className={s.search}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          aria-hidden="true"
+          focusable="false"
+          fill="black"
+          className={s.icon}
+        >
+          <path d="M10,18a8,8,0,1,1,8-8A8.009,8.009,0,0,1,10,18ZM10,4a6,6,0,1,0,6,6A6.007,6.007,0,0,0,10,4Z"></path>
+          <path d="M21,22a1,1,0,0,1-.707-0.293l-4-4a1,1,0,0,1,1.414-1.414l4,4A1,1,0,0,1,21,22Z"></path>
+        </svg>
+      </div>
     </div>
   );
 };
--- store/filters.tsx
+++ store/filters.tsx
@@ -2,12 +2,18 @@ import create from 'zustand';
 
 interface FilterStore {
   filter: string;
+  isFiltering: boolean;
   setFilter: (filter: string) => void;
+  setIsFiltering: (flag: boolean) => void;
 }
 
 export const useFilterStore = create<FilterStore>((set) => ({
   filter: '',
+  isFiltering: false,
   setFilter: (filter: string) => {
     set((state) => ({ ...state, filter: filter }));
+  },
+  setIsFiltering: (flag: boolean) => {
+    set((state) => ({ ...state, isFiltering: flag }));
   },
 }));
