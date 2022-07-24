f7e247fc8be887a1b2c1868719d1122b908a78c8
nhevia <nicolashevia@hotmail.com>
2022-07-24 13:31:30 +0200

    feat: open drawer for feedback on adding product to cart

--- components/cart/CartAdd/CartAdd.tsx
+++ components/cart/CartAdd/CartAdd.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useCartStore } from 'store/cart';
-import { ProductCart } from 'types/app';
 import { Dots } from '@components/ui/Loaders';
+import Drawer from '@components/ui/Drawer';
+import ProductAdded from '@components/products/ProductAdded';
+import { ProductCart } from 'types/app';
 import s from './CartAdd.module.css';
 
 interface AppProps {
@@ -11,6 +13,7 @@ interface AppProps {
 
 const CartAdd = ({ product, style }: AppProps) => {
   const [isButtonLoading, setIsButtonLoading] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   const addProduct = useCartStore((state) => state.addProduct);
 
@@ -19,18 +22,27 @@ const CartAdd = ({ product, style }: AppProps) => {
     setIsButtonLoading(true);
     setTimeout(() => {
       setIsButtonLoading(false);
+      setIsVisible(true);
     }, 300);
   };
 
   return (
-    <button
-      onClick={addToCart}
-      className={s['product_cart-button']}
-      style={style}
-      disabled={isButtonLoading}
-    >
-      {isButtonLoading ? <Dots /> : 'Add to cart'}
-    </button>
+    <>
+      {isVisible && (
+        <Drawer position="top" isBlurred={false} setVisible={setIsVisible}>
+          <ProductAdded product={product} setVisible={setIsVisible} />
+        </Drawer>
+      )}
+
+      <button
+        onClick={addToCart}
+        className={s['product_cart-button']}
+        style={style}
+        disabled={isButtonLoading}
+      >
+        {isButtonLoading ? <Dots /> : 'Add to cart'}
+      </button>
+    </>
   );
 };
 
