c769afe0a46e98edf459d6e72cb779f86bbd2e17
nhevia <nicolashevia@hotmail.com>
2022-07-25 09:47:04 +0200

    feat: option handler for Dropdown

--- components/ui/Dropdown/Dropdown.tsx
+++ components/ui/Dropdown/Dropdown.tsx
@@ -3,16 +3,28 @@ import s from './Dropdown.module.css';
 
 interface AppProps {
   options: Array<string | number>;
+  optionHandler: (category: string) => void;
   label?: string;
+  placeholder?: string;
 }
 
-const Dropdown = ({ options, label }: AppProps) => {
+const Dropdown = ({
+  options,
+  label,
+  placeholder = 'Select an option',
+  optionHandler,
+}: AppProps) => {
   return (
     <div className={s['dropdown-container']}>
       {label && <label className={s['dropdown-label']}>{label}</label>}
       <div className={s['dropdown-select-container']}>
-        <select className={s['dropdown-select']}>
-          <option value="selected">Select an option</option>
+        <select
+          className={s['dropdown-select']}
+          onChange={(e) => optionHandler(e.target.value.toLowerCase())}
+        >
+          <option selected value="">
+            {placeholder}
+          </option>
           {options.map((option) => (
             <option value={option} key={option}>
               {option}
