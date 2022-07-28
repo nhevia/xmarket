cef5be09c15f83fb5b96a64b8acb6eb6bda21a11
Nico Hevia <50466554+nhevia@users.noreply.github.com>
2022-07-28 13:41:05 +0200

    Pagination (#18)
    * style: fixes floating elements
    
    * feat: adds Pagination component

--- store/filters.tsx
+++ store/filters.tsx
@@ -3,17 +3,29 @@ import create from 'zustand';
 interface FilterStore {
   filter: string;
   isFiltering: boolean;
+  productsFiltered: number;
+  productsTotalAmount: number;
   setFilter: (filter: string) => void;
   setIsFiltering: (flag: boolean) => void;
+  setProductsFiltered: (amount: number) => void;
+  setProductsTotalAmount: (amount: number) => void;
 }
 
 export const useFilterStore = create<FilterStore>((set) => ({
   filter: '',
   isFiltering: false,
+  productsFiltered: 0,
+  productsTotalAmount: 0,
   setFilter: (filter: string) => {
     set((state) => ({ ...state, filter: filter.toLowerCase() }));
   },
   setIsFiltering: (flag: boolean) => {
     set((state) => ({ ...state, isFiltering: flag }));
+  },
+  setProductsFiltered: (amount: number) => {
+    set((state) => ({ ...state, productsFiltered: amount }));
+  },
+  setProductsTotalAmount: (amount: number) => {
+    set((state) => ({ ...state, productsTotalAmount: amount }));
   },
 }));
