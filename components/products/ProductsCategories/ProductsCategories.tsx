66148b3abc9a31d5e1fc1e589aa4a4273379708f
nhevia <nicolashevia@hotmail.com>
2022-07-31 20:50:23 +0200

    feat: add all categories link to category menu

--- components/products/ProductsCategories/ProductsCategories.tsx
+++ components/products/ProductsCategories/ProductsCategories.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useQueryClient } from 'react-query';
+import { useFilterStore } from 'store/filters';
 import { Dropdown } from 'components/ui';
 import useMediaQuery from 'hooks/useMediaQuery';
+import { Product } from 'types/app';
 import s from './ProductsCategories.module.css';
 import { categoriesConfiguration } from '__mocks__/categories';
 
 const ProductsCategories = () => {
   const matches = useMediaQuery('(max-width: 800px)');
   const router = useRouter();
+  const queryClient = useQueryClient();
+  const { setProductsTotalAmount } = useFilterStore((state) => state);
+
+  const handleApparelClick = () => {
+    const cachedProducts = queryClient.getQueryData<Product[]>('products');
+    cachedProducts && setProductsTotalAmount(cachedProducts.length);
+  };
 
   return (
     <>
@@ -26,7 +36,11 @@ const ProductsCategories = () => {
         </div>
       ) : (
         <>
-          <p className={s.title}>Categories</p>
+          <div onClick={handleApparelClick}>
+            <Link href="/products">
+              <p className={s.title}>All Categories</p>
+            </Link>
+          </div>
 
           {Object.keys(categoriesConfiguration).map((cat) => (
             <Link key={cat} href={`/products?search=${cat}`}>
