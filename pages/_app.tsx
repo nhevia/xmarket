309bd45063972fdaccb8b7453cace872b5aab8f4
nhevia <nicolashevia@hotmail.com>
2022-08-01 17:48:43 +0200

    fix: reset state after navigation from same page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import Layout from 'components/common/layout/Layout';
@@ -6,13 +7,14 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
         <Hydrate state={pageProps.dehydratedState}>
-          <Component {...pageProps} />
+          <Component key={router.asPath} {...pageProps} />
         </Hydrate>
         <ReactQueryDevtools initialIsOpen={false} />
       </Layout>
