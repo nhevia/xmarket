65c4d50e505c70c18e9b21dd9831ab0b1ebe3963
nhevia <nicolashevia@hotmail.com>
2022-08-01 23:03:33 +0200

    test: add missing queryClient

--- components/common/layout/Layout/Layout.test.tsx
+++ components/common/layout/Layout/Layout.test.tsx
@@ -1,12 +1,16 @@
 import { render } from '@testing-library/react';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import Layout from './Layout';
 
 describe('Layout', () => {
+  const queryClient = new QueryClient();
   it('renders the layout', () => {
     const { getByTestId } = render(
-      <Layout>
-        <div>Hello</div>
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <div>Hello</div>
+        </Layout>
+      </QueryClientProvider>
     );
 
     getByTestId('layout-main');
--- components/ui/Navbar/Navbar.test.tsx
+++ components/ui/Navbar/Navbar.test.tsx
@@ -1,12 +1,17 @@
-import Navbar from './Navbar';
 import { screen, render } from '@testing-library/react';
+import Navbar from './Navbar';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 describe('Navbar', () => {
-  beforeAll(() => {
-    render(<Navbar />);
-  });
+  const queryClient = new QueryClient();
 
   it('renders the navbar element', () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+      </QueryClientProvider>
+    );
+
     screen.getByLabelText('main navigation');
     expect(screen.getByRole('link', { name: 'Apparel' })).toHaveAttribute(
       'href',
